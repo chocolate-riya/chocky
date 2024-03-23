7765ddce3e481f8316bf5140ed3374e8ba4abb4f
chocolate-riya <pamarthi.priyanka559@gmail.com>
2024-03-23 11:35:41 +0000

    Update Pattern4.java

--- Pattern4.java
+++ Pattern4.java
@@ -18,3 +18,35 @@ class Pattern4
 		System.out.print("");
 	}
 }
+********************************************
+	# Define the URL of the folder you want to download
+$url = "https://webpaypg.jfrog.io/artifactory/docker-docker/gamutkart/"
+
+# Define the local directory where you want to save the downloaded folder
+$destinationFolder = "C:\Users\windows\fr"
+
+# Define your username and password
+$username = "webpay"
+$password = "Azuredemo@123"
+
+# Convert the username and password to a credential object
+$credentials = New-Object System.Management.Automation.PSCredential ($username, (ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+# Make a request to the URL with authentication
+$response = Invoke-WebRequest -Uri $url -Credential $credentials -Method Get
+
+# Check if the request was successful
+if ($response.StatusCode -eq 200) {
+    # Create the destination directory if it does not exist
+    if (-not (Test-Path -Path $destinationFolder)) {
+        New-Item -Path $destinationFolder -ItemType Directory -Force
+    }
+    
+    # Save the response content to a file
+    $response.Content | Out-File -FilePath "$destinationFolder\folder.zip" -Force
+
+    Write-Host "Folder downloaded successfully."
+} else {
+    Write-Host "Failed to download folder. Status code: $($response.StatusCode)"
+}
+
